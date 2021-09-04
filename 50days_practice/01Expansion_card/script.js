f4c0f95ada396f2d76ebb078d13ff2124e10c1f8
laerpeeK <56966563+laerpeeK@users.noreply.github.com>
2021-09-04 20:44:05 +0800

    0904

---
+++ 50days_practice/01Expansion_card/script.js
@@ -0,0 +1,9 @@
+const panelItems = document.querySelectorAll('.container > .panel')
+panelItems.forEach(item => {
+	item.addEventListener('click', () => {
+		[].filter.call(item.parentElement.children, el => el !== item).forEach(
+			el => el.classList.remove('active')
+		)
+		item.classList.add('active')
+	})
+})
