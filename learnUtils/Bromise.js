21f1086ab1fc72284c8743f6d7b556c76d37e736
laerpeeK <56966563+laerpeeK@users.noreply.github.com>
2021-09-06 20:51:54 +0800

    0906

---
+++ learnUtils/Bromise.js
@@ -0,0 +1,28 @@
+function Bromise(executor) {
+	let onResolve_ = null
+	let onReject_ = null
+
+	this.then = function(onResolve, onReject) {
+		onResolve_ = onResolve
+	}
+
+	function resolve(value) {
+		setTimeout(() => {
+			onResolve_(value)
+		}, 0)
+	}
+
+	executor(resolve, null)
+}
+
+function executor(resolve, reject) {
+	resolve(100)
+}
+
+let demo = new Bromise(executor)
+
+function onResolve(value) {
+	console.log(value)
+}
+
+demo.then(onResolve)
