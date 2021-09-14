0b9d09d385cf90722ee2f20749fb6bdfbef756d0
laerpeeK <56966563+laerpeeK@users.noreply.github.com>
2021-09-14 23:51:56 +0800

    0914

---
+++ learnA&D/data-structures/tree/BinaryTreeNode.js
@@ -0,0 +1,153 @@
+import Comparator from '../../utils/Comparator'
+import HashTable from '../hash-table/HashTable'
+
+export default class BinaryTreeNode {
+	constructor(value = null) {
+		this.left = null
+		this.right = null
+		this.parent = null
+		this.value = value
+
+		//任何与节点相关的元信息都可以存储在这里
+		this.meta = new HashTable()
+
+		//此比较器用于比较二叉树节点之间的差异
+		this.nodeComparator = new Comaprator()		
+	}
+
+	get leftHeight() {
+		if(!this.left) {
+			return 0
+		}
+
+		return this.left.height + 1
+	}
+
+	get rightHeight() {
+		if(!this.right) {
+			return 0
+		}
+		return this.right.height + 1
+	}
+
+	get height() {
+		return Math.max(this.leftHeight, this.rightHeight)
+	}
+
+	get blanceFactor() {
+		return this.leftHeight - this.rightHeight
+	}
+
+	get uncle() {
+		if(!this.parent) {
+			return undefined
+		}
+
+		if(!this.parent.parent) {
+			return undefined
+		}
+
+		if(!this.parent.parent.left || this.parent.parent.right) {
+			return undefined
+		}
+
+		//通过上述判断，我们知道当前节点有祖父母节点且祖父母节点有两个孩子节点，
+		//接着找谁是它的同代节点
+		if(this.nodeComparator.equal(this.parent, this.parent.parent.left)) {
+			return this.parent.parent.right
+		}
+
+		return this.parent.parent.left
+	}
+
+	setValue(value) {
+		this.value = value
+		return this
+	}
+
+	setLeft(node) {
+		if (this.left) {
+			this.left.parent = null
+		}
+
+		this.left = node
+
+		if(this.left) {
+			this.left.parent = this
+		}
+
+		return this
+	}
+
+	setRight(node) {
+		if(this.right) {
+			this.right.parent = null
+		}
+
+		this.right = node
+
+		if(node) {
+			this.right.parent = this
+		}
+
+		return this
+	}
+
+	removeChild(nodeToRemove) {
+		if(this.left && this.nodeComparator.equal(this.left, nodeToRemove)) {
+			this.left = null
+			return true
+		}
+
+		if(this.right && this.nodeComparator.equal(this.right, nodeToRemove)) {
+			this.right = null
+			return true
+		}
+
+		return false
+	}
+
+	replaceChild(nodeTOReplace, replacementNode) {
+		if(!nodeToReplace || !replacementNode) {
+			return false
+		}
+
+		if(this.left && this.nodeComparator.equal(this.left, nodeToReplace)) {
+			this.left = replacementNode
+			return true
+		}
+
+		if(this.right && this.nodeComparator.equal(this.right, nodeToReplace)) {
+			this.right = replacementNode
+			return true
+		}
+
+		return false
+	}
+
+	static copyNode(sourceNode, targetNode) {
+		targetNode.setValue(sourceNode.value)
+		targetNode.setLeft(sourceNode.left)
+		targetNode.setRight(sourceNode.right)
+	}
+
+	traverseInOrder() {
+		let traverse = []
+
+		if(this.left) {
+			traverse = traverse.concat(this.left.traverseInorder())
+		}
+
+		traverse.push(this.value)
+
+		if(this.right) {
+			traverse = traverse.concat(this.right.traverseInOrder())
+		}
+		
+		return traverse
+	}
+
+	toString() {
+		return this.traverseInOrder().toString()
+	}
+}
---
+++ learnA&D/data-structures/tree/binary_search_tree/BinarySearchTreeNode.js
@@ -0,0 +1,112 @@
+import BinaryTreeNode from '../BinaryTreeNode'
+import Comparator from '../../../utils/Comparator'
+
+export default class BinarySearchTreeNode extends BinaryTreeNode {
+	constructor(value = null, compareFunction = undefined) {
+		super(value) //node value
+
+		this.compareFunction = compareFunction //comparator function for node values
+
+		//此比较器用于相互比较节点值
+		this.nodeValueComparator = new Comparator(compareFunction)
+	}
+
+	insert(value) {
+		if(this.nodeValueComparator.equal(this.value, null)) {
+			this.value = value
+
+			return this
+		}
+
+		if(this.nodeValueComparator.lessThan(value, this.value)) {
+			if(this.left) {
+				return this.left.insert(value)
+			}
+
+			const newNode = new BinarySearchTreeNode(value, this.compareFunction)
+			this.setLeft(newNode)
+
+			return newNode
+		}
+		
+		if(this.nodeValueComparator.greaterThan(value, this.value)) {
+			if(this.right) {
+				return this.right.insert(value)
+			}
+
+			const newNode = new BinarySearchTreeNode(value, this.compareFunction)
+	    this.setRight(newNode)
+
+			return newNode
+		}
+		
+		return this
+	}
+
+	find(value) {
+		if(this.nodeValueComparator.equal(this.value, value)) {
+			return this
+		}
+
+		if(this.nodeValueComparator.lessThan(value, this.value) && this.left) {
+			return this.left.find(value)
+		}
+
+		if(this.nodeValueComparator.greaterThan(value, this.value) && this.right) {
+			return this.right.find(value)
+		} 
+
+		return null
+	}
+
+	contains(value) {
+		return !!this.find(value)
+	}
+
+	remove(value) {
+		const nodeToRemove = this.find(value)
+
+		if(!nodeToRemove) {
+			throw new Error('Item not found in the tree')
+		}
+
+		const { parent } = nodeToRemove
+
+		if(!nodeToRemove.left && !nodeToRemove.right) {
+			if(parent) {
+				parent.removeChild(nodeToRemove)
+			} else {
+				nodeToRemove.setValue(undefined)
+			}
+		} else if(nodeToRemove.left && nodeToRemove.right) {
+			const nextBiggerNode = nodeToRemove.right.findMin()
+			if(!this.nodeComparator.equal(nextBiggerNode, nodeToRemove.right)) {
+				this.remove(nextBiggerNode.value)
+				nodeToRemove.setValue(nextBiggerNode.value)
+			} else {
+				nodeToRemove.setValue(nodeToRemove.right.value)
+				nodeToRemove.setRight(nodeToRemove.right.right)
+			}
+		} else {
+			const childNode = nodeToRemove.left || ndoeToRemove.right
+
+			if(parent) {
+				parent.replaceChild(nodeToRemove, childNode)
+			} else {
+				BinaryTreeNode.copyNode(childNode, nodeToRemove)
+			}
+		}
+
+		nodeToRemove.parent = null
+
+		return true
+	}
+
+	findMin() {
+		if(!this.left) {
+			return this
+		}
+
+		return this.left.findMin()
+	}
+}
