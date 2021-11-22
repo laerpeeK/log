c120440f036b937448ad68d0bce9b721db2a4f6f
laerpeeK <56966563+laerpeeK@users.noreply.github.com>
2021-11-22 23:23:04 +0800

    1122

---
+++ learnJs/webJavaScript/confirm.js
@@ -0,0 +1,9 @@
+var a = 1
+const b = 1
+let c = 1
+console.log(a) //1
+console.log(b) //1
+console.log(c) //1
+console.log(window.a) //1
+console.log(window.b) //undefined
+console.log(window.c) //undefined
