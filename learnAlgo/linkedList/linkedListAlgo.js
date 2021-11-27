68321610a2dad5e4afac61131d95ce87ddd25144
laerpeeK <56966563+laerpeeK@users.noreply.github.com>
2021-11-27 18:30:49 +0800

    1127

--- learnAlgo/linkedList/linkedListAlgo.js
+++ learnAlgo/linkedList/linkedListAlgo.js
@@ -3,15 +3,33 @@ class newLinkedList extends LinkedList {
 	constructor() {
 		super()
 	}
+
+	//单链表反转
+
+	//时间复杂度On,空间复杂度O1
 	reverseList() {
 		const root = new Node('head')
 		let currentNode = this.head.next
 		while (currentNode !== null) {
 			const next = currentNode.next
-			currentNode.next = root.next
+			currentNode.next = root.next  //巧妙之处
 			root.next = currentNode
 			currentNode = next
 		}
 		this.head = root
+	}
+
+
+	//环验证
+	//时间复杂度On,空间复杂度O1
+	checkCircle() {
+		let fast = this.head.next
+		let slow = this.head
+		while (fast !== null && fast.next !== null) {
+			fast = fast.next.next
+			slow = slow.next
+			if (fast === slow) return true
+		}
+		return false
 	}
 }
