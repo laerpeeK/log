4f0e9db56e0e84855d90cb4619d8a660c34683ac
laerpeeK <56966563+laerpeeK@users.noreply.github.com>
2021-11-28 15:23:16 +0800

    1128

--- learnAlgo/linkedList/linkedListAlgo.js
+++ learnAlgo/linkedList/linkedListAlgo.js
@@ -50,4 +50,58 @@ class newLinkedList extends LinkedList {
 		this.remove(curretNode.element)
 		this.reverseList()
 	}
+
+	//求中间节点
+	//时间复杂度On,空间复杂度O1
+	findmMiddleNode() {
+		let fast = this.head
+		let slow = this.head
+		while (fast.next !== null && fast.next.next !== null) {
+			fast = fast.next.next
+			slow = slow.next
+		}
+		return slow
+	}
+}
+
+//两个有序链表合并
+const mergeSortedLists = (listA, listB) => {
+	if (!listA) {
+		return listB
+	} 
+	if (!listB) {
+		return listA
+	}
+
+	let a = listA
+	let b = listB
+	let resultList = undefined
+
+	if (a.element < b.element) {
+		returnList = a
+		a = a.next
+	} else {
+		resultList = b
+		b = b.next
+	}
+
+	let currentNode = resultList
+	
+	while (a!==null && b!== null) {
+		if (a.element < b.element) {
+			currentNode.next = a
+			a = a.next
+		} else {
+			currentNode.next = b
+			b = b.next
+		}
+		currentNode = currentNode.next
+	}
+
+	if (a !== null) {
+		currentNode.next = a
+	} else {
+		currentNode.next = b
+	}
+	return resultList
 }
