ecac8d1a5e731a80ee37e7019420ecff8a2122bb
laerpeeK <56966563+laerpeeK@users.noreply.github.com>
2021-11-30 08:49:04 +0800

    1130

--- learnAlgo/sort/sort.js
+++ learnAlgo/sort/sort.js
@@ -13,5 +13,36 @@ const bubbleSort = (arr) => {
 	console.log(arr) 
 }
 
+const insertSort = (arr) => {
+	if (arr.length <= 1) return
+	for (let i = 1; i < arr.length; i++) {
+		const temp = arr[i]
+		let j = i - 1
+		for (j;j >= 0; j--) {
+			if (arr[j] > temp) {
+				arr[j + 1] = arr[j]
+			} else {
+				break;
+			}
+		}
+		arr[j+1] = temp
+	}
+	console.log(arr)
+} 
 
+const selectionSort = (arr) => {
+	if (arr.length <= 1) return 
+	for (let i = 0; i < arr.length - 1; i++) {
+		let minIndex = i
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[minIndex]) {
+				minIndex = j 
+			}
+		}
+		const temp = arr[i]
+		arr[i] = arr[minIndex]
+		arr[minIndex] = temp
+	}
+	console.log(arr)
+}
 
