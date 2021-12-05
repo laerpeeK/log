6d9ea1ab5569d3c10519746d14817c41a4f848ae
laerpeeK <56966563+laerpeeK@users.noreply.github.com>
2021-12-05 17:32:04 +0800

    1205

--- learnAlgo/binary/binaryFind2.js
+++ learnAlgo/binary/binaryFind2.js
@@ -19,4 +19,20 @@ const binaryFindFirst = (sortedArr, target) => {
 }
 
 
-//查找第一个大于等于给定值的元素
+//查找最后一个相等的数
+const binaryFindLast = (sortedArr, target) => {
+	if (sortedArr.length === 0) return -1 
+	let low = 0
+	let high = sortedArr.length - 1
+	while (low <= high) {
+		const mid = Math.floor((low + high) / 2)
+		if (target < sortedArr[mid]) {
+			high = mid - 1
+		} else if (target > sortedArr[mid]) {
+			low = mid + 1
+		} else {
+			if (mid === sortedArr.length - 1 || sortedArr[mid + 1] > target)  return mid
+			low = mid + 1 
+		}
+	}
+}
